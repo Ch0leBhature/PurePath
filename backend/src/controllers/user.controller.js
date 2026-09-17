501feabdaf4011bca59f7e0ea83c773abb8b0b6b
Ch0leBhature <225065954+Ch0leBhature@users.noreply.github.com>
2026-09-17 14:13:26 +0530

    cokkie problem

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -17,6 +17,7 @@ const cookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
   sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  partitioned: process.env.NODE_ENV === "production",
 };
 
 const generateAccessAndRefreshTokens = async function (userId) {
